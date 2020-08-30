25e52e7f7201178994d690115098d9ce85232cbe
tony·parker <15874751507@163.com>
2020-08-30 22:46:27 +0800

    init design pattern

---
+++ src/main/java/designPartterns/adapter/ducks/Duck.java
@@ -0,0 +1,6 @@
+package designPartterns.adapter.ducks;
+
+public interface Duck {
+	public void quack();
+	public void fly();
+}
---
+++ src/main/java/designPartterns/adapter/ducks/Turkey.java
@@ -0,0 +1,6 @@
+package designPartterns.adapter.ducks;
+
+public interface Turkey {
+	public void gobble();
+	public void fly();
+}
