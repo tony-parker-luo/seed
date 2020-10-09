c63220f5b58708e5cfee7f77d9842ae4e21661ba
Calen <15874751507@163.com>
2020-10-09 09:13:04 +0800

    update http

---
+++ src/main/java/http/HttpClient.java
@@ -0,0 +1,21 @@
+package http;
+
+/**
+ * @ClassName HttpClient.java
+ * @Description HTTP Client
+ * @Author Galen Luo
+ * @Version 1.0.0
+ * @Date 2020/9/30 9:17
+ */
+public class HttpClient {
+
+    /**
+     * @description HTTP POST
+     * @author Galen Luo
+     * @date 2020/9/30 9:41
+     */
+    public void post(String a) {
+        
+    }
+
+}
---
+++ src/main/java/http/HttpMethod.java
@@ -0,0 +1,13 @@
+package http;
+
+/**
+ * @ClassName HttpMethod.java
+ * @Description TODO
+ * @Author Calen Luo
+ * @Version 1.0.0
+ * @Date 2020/9/30 9:42
+ */
+public interface HttpMethod {
+
+
+}
